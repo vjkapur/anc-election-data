4a4855cd3d5a74b2d961407893a4043362fac279
VJ Kapur <vj@intrical.us>
2024-11-10 04:49:17 -0500

    lots of catch-up stuff and new results

--- compute-turnout.py
+++ compute-turnout.py
@@ -9,6 +9,8 @@ votes_2022 = pd.read_csv('data/certified-votes-20221108.csv')
 votes_2022.ContestName = votes_2022.ContestName.str.replace('8F0', '6/8F0')
 voters_2022.SMD = voters_2022.SMD.str.replace('8F0', '6/8F0')
 
+votes_2024 = pd.read_csv('data/2024/November_5_2024_General_Election_Election_Night_Unofficial_Results.csv')
+
 # initialize a Dataframe with one row per SMD
 voter_counts = pd.DataFrame()
 voter_counts['SMD'] = voters_2024.SMD.unique()
@@ -18,9 +20,14 @@ voter_counts = voter_counts.sort_values('SMD')
 voter_counts['2022-10-19 reg'] = voters_2022.SMD.value_counts()[voter_counts.SMD].values
 voter_counts['2024-09-04 reg'] = voters_2024.SMD.value_counts()[voter_counts.SMD].values
 voter_counts['2022-11-08 cast'] = [votes_2022[votes_2022.ContestName.str.contains(smd)].Votes.sum() for smd in voter_counts.SMD]
-all_2022 = votes_2022.Votes.sum()
+all_2022 = votes_2022[votes_2022.ContestName.str.contains('DELEGATE TO THE HOUSE OF REPRESENTATIVES')].Votes.sum()
 voter_counts['2022-11-08 pct of whole'] = voter_counts['2022-11-08 cast'] / all_2022 * 100
 voter_counts['2022 turnout'] = voter_counts['2022-11-08 cast'] / voter_counts['2022-10-19 reg'] * 100
+voter_counts['2024-11-05 prelim'] = [votes_2024[votes_2024.ContestName.str.contains(smd)].Votes.sum() for smd in voter_counts.SMD]
+voter_counts['2024 prelim turnout'] = voter_counts['2024-11-05 prelim'] / voter_counts['2024-09-04 reg'] * 100
+all_2024 = votes_2024[votes_2024.ContestName.str.contains('ELECTORS OF PRESIDENT AND VICE PRESIDENT OF THE UNITED STATES')].Votes.sum()
+voter_counts['2024-11-05 pct of whole'] = voter_counts['2024-11-05 prelim'] / all_2024 * 100
+voter_counts['prop of prop'] = voter_counts['2024-11-05 pct of whole'] / voter_counts['2022-11-08 pct of whole']
 
 # write it out to a CSV
 voter_counts.to_csv('voter_counts_by_smd.csv', index=False, header=True)
--- process-results.py
+++ process-results.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import numpy as np
 from os import listdir
 from os.path import isfile, join
 
@@ -29,6 +30,7 @@ def print_summary_snapshot(old_result, new_result, competitive_races):
         columns=['SMD', 'candidates', 'previous_margin', 'new_ballots', 'new_margin'])
     summary.SMD = competitive_races.keys()
     for smd in competitive_races:
+        print('checking smd %s'%smd)
         summary.loc[summary.SMD == smd, 'candidates'] = "%s/%s" % (
             competitive_races[smd][0], competitive_races[smd][1])
         summary.loc[summary.SMD == smd, 'previous_margin'] = get_smd_margin(
@@ -41,35 +43,42 @@ def print_summary_snapshot(old_result, new_result, competitive_races):
     summary.sort_values('new_margin', ascending=False, inplace=True)
     print(summary.to_string(index=False))
 
-#revisions = ['tues8', 'wed9', 'thurs10', 'fri11', 'mon14', 'fri18', 'mon21', 'tues22', 'wed30']
-revisions = [f for f in listdir('data/2024/') if isfile(join('data/2024/', f))]
+revisions = ['2024-11-05 (1).csv', '2024-11-05 (2).csv', '2024-11-07.csv', '2024-11-08.csv', '2024-11-09.csv']
+# revisions = reversed([f for f in listdir('data/2024/') if (('.csv' in f) and isfile(join('data/2024/', f)))])
 results = dict.fromkeys(revisions)
 
-yimbyness = pd.read_csv('data/yimbyness.csv')
+yimbyness = pd.read_csv('data/yimbyness.csv',dtype={"SMD":'U'})
 Y_grouping = yimbyness.groupby('SMD').sum()
 competitive_SMDs_with_Y = Y_grouping[Y_grouping.score > 0].index.values
 
-competitive_races = dict.fromkeys(competitive_SMDs_with_Y, [])
+voter_counts = pd.read_csv('voter_counts_by_smd.csv')
+ballots_2022 = pd.read_csv('2022_ballots_by_smd_over_time.csv')
+ballots_2022.SMD = ballots_2022.SMD.str.replace('8F0', '6/8F0')
+ballots_2022['residual-2022'] = ballots_2022['wed30 ballots'] - ballots_2022['tues8 ballots']
 
-# competitive_races = {'5D06': ['McCray', 'Henderson'],
-#                     '5B07': ['Rathore', 'Haynes'],
-#                     '5E01': ['Gardiner', 'Rodriguez'],
-#                     '5E05': ['Lopez', 'Thompson'],
-#                     '5B05': ['Lopez', 'Feeley']}
+# competitive_races = dict.fromkeys(competitive_SMDs_with_Y, [])
+
+competitive_races = {'1B02': ['Barrilleaux', 'Chauvin'],
+                    '3A03': ['Bowers', 'Perisic'],
+                    '3/4G05': ['Simmons', 'Rooney'],
+                    '5E05': ['Lopez', 'Thompson'],
+                    '6/8F02': ['Bryant', 'De Souza']}
 
 for revision in revisions:
     results[revision] = pd.read_csv('data/2024/%s' % revision)
 
-print_summary_snapshot(results[revisions[-2:][0]],
-                       results[revisions[-1:][0]], competitive_races)
-
 # get a list of ANC SMDs
 anc_contests = results[next(iter(results))][results[next(iter(results))].ContestName.str.contains(
-    'ANC - ')].ContestName.unique()
-smds = [contest.split(' ')[2] for contest in anc_contests]
+    'ANC-')].ContestName.unique()
+smds = [contest.split('-')[1] for contest in anc_contests]
+
+# print(smds)
 
 # sort the list
 smds.sort()
+
+print_summary_snapshot(results[revisions[-2:][0]],
+                       results[revisions[-1:][0]], competitive_races)
 
 # set up a dataframe for counts by SMD per data revision
 revision_metrics = []
@@ -89,9 +98,11 @@ margin_counts.SMD = competitive_races.keys()
 
 # for each SMD and revision, calculate the ballot count
 for smd in smds:
-    competitive_races[smd][0] = yimbyness[yimbyness.SMD == smd].sort_values(by='score', ascending=False).candidate.iloc[0]
-    competitive_races[smd][1] = yimbyness[yimbyness.SMD == smd].sort_values(by='score', ascending=False).candidate.iloc[1]
- 
+#     if smd in competitive_races.keys():
+#         competitive_races[smd].append(yimbyness[yimbyness.SMD == smd].sort_values(by='score', ascending=False).candidate.iloc[0])
+#         competitive_races[smd].append(yimbyness[yimbyness.SMD == smd].sort_values(by='score', ascending=False).candidate.iloc[1])
+#         print("smd %s, candidates: %s"%(smd, competitive_races[smd][0]))
+
     for revision in revisions:
         ballot_counts.loc[ballot_counts.SMD == smd,
                           '%s ballots' % revision] = get_smd_ballot_count(results[revision], smd)
@@ -105,6 +116,12 @@ for smd in smds:
                               revision] = get_smd_margin(results[revision], smd, competitive_races[smd])
             margin_counts.loc[margin_counts.SMD == smd, 'margin-pct-%s' %
                               revision] = get_smd_margin(results[revision], smd, competitive_races[smd])/get_smd_ballot_count(results[revision], smd)
+
+# not sure what's up here
+ballot_counts.SMD = ballot_counts.SMD.str.replace('6/8F01 6/8F01', '6/8F01')
+ballot_counts = ballot_counts.merge(ballots_2022[['SMD', 'residual-2022']], how='left', on='SMD')
+ballot_counts['residual-2024'] = ballot_counts['2024-11-09.csv ballots'] - ballot_counts['2024-11-05 (1).csv ballots']
+ballot_counts['expected remaining ballots'] = ballot_counts['residual-2022'] * 1.5 - ballot_counts['residual-2024']
 
 # write it out to a CSV
 ballot_counts.to_csv('ballots_by_smd_over_time.csv', index=False, header=True)
